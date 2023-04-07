200d149b9cda8bbf148dc4fcfc4a06e2f696c9f9
ahyk03070811 <126743881+ahyk03070811@users.noreply.github.com>
2023-04-07 11:39:49 +0000

    商品新規登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,11 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!,only: [:create,:edit,:update,:index, :show, :new]
+
   def index
   end
 
   def new
+    @item=Item.new
   end
 
   def show
@@ -10,4 +13,12 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
+  
+  
+  
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :image, :is_active)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,15 @@ class Item < ApplicationRecord
   has_many :order_details
   
   has_one_attached :image
+  
+  def get_image(width,height)
+    unless image.attached?
+      file_path=Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io:File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [100,100]).processed
+  end
+  
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true

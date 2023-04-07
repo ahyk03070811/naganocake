f804a81537f8a2773af8f7f1f7fc977621cd2c19
ahyk03070811 <126743881+ahyk03070811@users.noreply.github.com>
2023-04-07 09:10:19 +0000

    ジャンル追加、一覧画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :genre
+  has_many :cart_items
+  has_many :order_details
+  
+  has_one_attached :image
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+ 
+  namespace :admin do
+    get 'admin' => 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :show, :new, :create, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
+    resources :order_details, only: [:update]
+  end
+  
+  
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  scope module: 'customers' do
+    root 'homes#top'
+    get 'about' => 'homes#about'
+   end
+  
  devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'

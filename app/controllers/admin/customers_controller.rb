4975eaed39f947d12f98d67b7fcf73ac88654299
ahyk03070811 <126743881+ahyk03070811@users.noreply.github.com>
2023-04-10 11:23:36 +0000

    管理者トップページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -11,6 +11,16 @@ class Admin::CustomersController < ApplicationController
     @customer=Customer.find(params[:id])
   end
   
+  def update
+    @customer=Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice]="会員情報を更新しました"
+      redirect_to admin_customer_path
+    else
+      render :edit
+      
+  end
+  
   
  private
   def customer_params
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
+    @orders=Order.where("created_at"===Date.today)
+    @order=Order.page(params[:page]).reverse_order
   end
 end

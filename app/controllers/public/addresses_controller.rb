f8d73033857cc3ad1aadb29391dde9640ac8d57b
ahyk03070811 <126743881+ahyk03070811@users.noreply.github.com>
2023-04-15 03:40:25 +0000

    配送先画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,7 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = current_member.addresses.page(params[:page]).per(3)
+  	@address = Address.new
   end
 
   def edit
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,8 +4,32 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer=Cistomer.find(params[:id])
   end
 
-  def unsubscribe
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "個人情報を編集しました"
+      redirect_to customers_may_page(current_customer.id)
+    else
+      flash[:danger] = "個人情報の編集に失敗しました"
+      render :edit
+    end
   end
+
+  def withdraw
+    current_customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :telephone_number, :postal_code, :address, :is_deleted)
+  end
+
+
 end

c13b400068f6999ce7a617e28f06c59d57b7339f
ahyk03070811 <126743881+ahyk03070811@users.noreply.github.com>
2023-04-22 07:42:05 +0000

    カート内商品

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
+    @quantity=Item.count
+    @items=Item.all.page(params[:page]).per(8)
   end
 
   def show
+    @item=Item.find(params[:id])
+    @cart_item=CartItem.new
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :customer
-  
-  validates :postcode, presence: true, length: { maximum: 7, minimun: 7 },numericality: true
+
+  validates :postal_code, presence: true, length: { maximum: 7, minimun: 7 },numericality: true
 	validates :address, presence: true
 	validates :name, presence: true
-	
+
 end
